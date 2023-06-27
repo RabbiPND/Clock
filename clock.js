d2f56a48c0c57bc1063f686e74075b3146823a42
Rabbipxd <130862563+RabbiPND@users.noreply.github.com>
2023-06-27 21:51:22 +0200

    final clock project
    final clock project

--- clock.js
+++ clock.js
@@ -1,18 +1,24 @@
 function updateClock() {
-    const now = new Date ()
+    const currentDate = new Date ()
+    console.log(currentDate)
 
-    const hours = now.getHours()
-    const minutes = now.getMinutes()
-    const seconds = now.getSeconds()
+    const hours = currentDate.getHours()
+    console.log(hours)
+    const minutes = currentDate.getMinutes()
+    console.log(minutes)
+    const seconds = currentDate.getSeconds()
+    console.log(seconds)
 
     let hourHand = document.querySelector(".hours")
     let minuteHand = document.querySelector(".minutes")
     let secondHand = document.querySelector(".seconds")
+    let newDate = document.getElementById("new-date")
 
     //calculate the rotation angles
-    let hourRotation = (hours % 12) * 30 + minutes / 2;
-    let minuteRotation = minutes * 6;
-    let secondRotation = seconds * 6;
+    let hourRotation = (hours / 12) * 360; // 12 for the clock and 360 because of the size of the watch//
+    let minuteRotation = (minutes / 60) * 360; //60 minutes in an hour
+    let secondRotation = (seconds / 60) * 360; //60 seconds in a minute
+    newDate.textContent = currentDate
 
     // Apply the rotation to the clock hands
     hourHand.style.transform = `rotate(${hourRotation}deg)`;
